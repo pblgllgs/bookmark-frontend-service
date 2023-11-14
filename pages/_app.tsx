55378db637e20197bb4f840ec5d9fe170a240e85
pblgllgs <pbl.gllgs@gmail.com>
2023-11-14 16:52:13 -0300

    add navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import type { AppProps } from 'next/app'
+import NavBar from '../components/NavBar'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NavBar />
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
+
+  )
 }
 
 export default MyApp
